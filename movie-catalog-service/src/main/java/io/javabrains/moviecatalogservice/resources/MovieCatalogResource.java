b7197a4f4641f4a75a6532f95f074a21bb186dbd
System Administrator <root@Maxs-MacBook-Pro.local>
2019-04-27 19:31:16 -0400

    getting microservics together with WebClient

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -24,9 +25,12 @@ public class MovieCatalogResource {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
     @GetMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
-        
+
         //get all rated movie IDs
         List<Rating> ratings = Arrays.asList(
                 new Rating("1234",4),
@@ -35,8 +39,15 @@ public class MovieCatalogResource {
 
         //For each movie ID, call movie info service and get details
         return ratings.stream().map(rating -> {
-            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
-          return   new CatalogItem(movie.getName(),"Test",rating.getRating());
+            //Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+
+            Movie movie = webClientBuilder.build()
+                    .get()
+                    .uri("http://localhost:8082/movies/" + rating.getMovieId())
+                    .retrieve()
+                    .bodyToMono(Movie.class)
+                    .block();
+            return   new CatalogItem(movie.getName(),"Test",rating.getRating());
         }).collect(Collectors.toList());
 
 
