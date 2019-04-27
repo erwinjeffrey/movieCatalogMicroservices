3baa7432dca6d385c42da817d91354747bd649d4
System Administrator <root@Maxs-MacBook-Pro.local>
2019-04-27 19:09:08 -0400

    getting microservics together with Restemplate

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,9 +1,11 @@
 package io.javabrains.moviecatalogservice.resources;
 
+import com.sun.org.apache.regexp.internal.RE;
 import io.javabrains.moviecatalogservice.models.CatalogItem;
 import io.javabrains.moviecatalogservice.models.Movie;
 import io.javabrains.moviecatalogservice.models.Rating;
 import org.hibernate.validator.constraints.SafeHtml;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,24 +21,25 @@ import java.util.stream.Collectors;
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @GetMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
-
-        RestTemplate restTemplate = new RestTemplate();
-
+        
         //get all rated movie IDs
         List<Rating> ratings = Arrays.asList(
                 new Rating("1234",4),
                 new Rating("5678",3)
         );
 
+        //For each movie ID, call movie info service and get details
         return ratings.stream().map(rating -> {
             Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
           return   new CatalogItem(movie.getName(),"Test",rating.getRating());
-        })
-                .collect(Collectors.toList());
+        }).collect(Collectors.toList());
 
-        //For each movie ID, call movie info service and get details
+
 
         //Put them all together
            /// return Collections.singletonList(
